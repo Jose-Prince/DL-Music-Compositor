ae4e47f452bf2112145d78f553de65ee066538d7
Jose-Prince <joseprince148@gmail.com>
2025-07-09 09:28:50 -0600

    Fix: correction in training NN

--- main.m
+++ main.m
@@ -2,14 +2,23 @@
 midi = readmidi('/MATLAB Drive/DL-Music-Compositor/giantmidi-piano/Bach_Prelude_and_Fugue_in_A-flat_major_BWV862_gCL5Zvnt0TU_a.mid');
 notes = midiInfo(midi, 0);
 
-X = notes(1:end-1);
-Y = notes(2:end);
+pitches = notes(:, 3);
+pitches = pitches - 20;
+pitches = pitches(pitches >= 1 & pitches <= 88);
+
+X = pitches(1:end-1)';
+Y = pitches(2:end)';
+
+XTrain = {X};
+YTrain = {categorical(Y)};
+
+numClasses = numel(unique(Y));
 
 % Neunoral network
 layers = [
     sequenceInputLayer(1)
     lstmLayer(100)
-    fullyConnectedLayer(88)
+    fullyConnectedLayer(numClasses)
     classificationLayer];
 
 options = trainingOptions('adam', ...
@@ -20,14 +29,15 @@ options = trainingOptions('adam', ...
     'Plots','training-progress');
 
 % Training
-net = trainingNetwork(XTrain, YTrain, layers, options);
+net = trainNetwork(XTrain, YTrain, layers, options);
 
 % Generate music
-note = 60;
+note = 40;
 sequence = [];
 
 for i = 1:100
-    [pred, scores2label] = classify(net, note);
-    note = pred;
-    sequence = [sequence; note];
+    input = {note};
+    pred = classify(net, input);
+    note = double(pred{1});
+    sequence(end+1) = note;
 end
